faf6ae18e1c07ae211217f8eab2452da509a8215
SidneyPiper <sidlapip@gmail.com>
2024-08-05 14:40:31 +0200

    browser...

--- core/Browser.ts
+++ core/Browser.ts
@@ -1,3 +1,11 @@
 import puppeteer from "puppeteer";
 
-export default async () => puppeteer.launch();
+export default async () => puppeteer.launch({
+    executablePath: process.env.BROWSER,
+    headless: true,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-gpu'
+    ]
+});
